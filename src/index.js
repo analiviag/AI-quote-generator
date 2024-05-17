e219245a5243d4dd71e9b78911b259e08ed70abb
analiviag <126291217+analiviag@users.noreply.github.com>
2024-05-17 16:32:24 -0300

    added basic JS and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function quoteForm(event) {
+  event.preventDefault();
 
+  new Typewriter("#text-quote", {
+    strings:
+      "Life is like a box of chocolates. You never know what you're gonna get.",
+    autoStart: true,
+    delay: 10,
+    cursor: null,
+  });
+}
+
+let quoteGenerator = document.querySelector("#quote-generator-form");
+quoteGenerator.addEventListener("submit", quoteForm);
