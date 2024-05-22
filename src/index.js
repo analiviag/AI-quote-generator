6bc1a8e7d3cd513629ac7b43375d4379301242ed
analiviag <126291217+analiviag@users.noreply.github.com>
2024-05-22 12:54:52 -0300

    added  AI to js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-function quoteForm(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#text-quote", {
-    strings:
-      "Life is like a box of chocolates. You never know what you're gonna get.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: null,
   });
 }
 
+function quoteForm(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "bba264o26c1cdt003d73631d7a2a1f27";
+  let context =
+    "You are a quote generator, that gives great quotes. Give me the answer in HTML format. Please always add the name of the author of the quote, wrapped in a <strong> element. The name of the author should ALWAYS be in a new line.";
+  let prompt = `Write a quote about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
+}
 let quoteGenerator = document.querySelector("#quote-generator-form");
 quoteGenerator.addEventListener("submit", quoteForm);
